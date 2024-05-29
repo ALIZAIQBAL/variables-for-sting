d7e338eeff6dfcd53ecd0692870197dbd8a32a00
ALIZAIQBAL <alizaiqbal322@gmail.com>
2024-05-29 05:49:12 -0700

    variables for string

---
+++ app.js
@@ -0,0 +1,23 @@
+var username;
+var myName="Your Full Name";
+
+
+var message;
+message="Hello World";
+alert(message);
+
+var studentName="John Doe";
+var age="15 years old";
+var course="Certified Mobile Application Development";
+alert(studentName);
+alert(age);
+alert(course);
+var message="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(message);
+var email="example@example.com";
+alert("My email adress is "+ email);
+var book="A smarter way to learn JavaScript";
+alert("Iam trying to learn the book" + book);
+document.write("Yah! I can write HTML content through JavaScript");
+var specialCharacters="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialCharacters);
